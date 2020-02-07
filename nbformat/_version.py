ac7d30723c2ff8459bf8fa9c51977126ed2f987c
amangarg96 <aman.garg1996@gmail.com>
2020-02-07 18:11:24 +0530

    versioning with suffix dev

--- nbformat/_version.py
+++ nbformat/_version.py
@@ -1,4 +1,4 @@
 # Make sure to update package.json, too!
 # version_info = (4, 5, 0, 'dev')
-version_info = (4, 4, 0)
+version_info = (4, 4, 1, 'dev2')
 __version__ = '.'.join(map(str, version_info))
