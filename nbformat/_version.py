3456d18633d77a453905905f2bcda7830f88770c
amangarg96 <aman.garg1996@gmail.com>
2020-02-11 14:26:06 +0530

    removed python version check from metadata

--- nbformat/_version.py
+++ nbformat/_version.py
@@ -1,4 +1,4 @@
 # Make sure to update package.json, too!
 # version_info = (4, 5, 0, 'dev')
-version_info = (4, 4, 1, 'dev2')
+version_info = (4, 4, 1, 'dev3')
 __version__ = '.'.join(map(str, version_info))
